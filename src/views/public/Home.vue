524da670b40ec614a264888c9b0b5d5ea370f2ca
gbehouenou <gbehouenousilvio@gmail.com>
2025-12-18 06:46:00 +0100

    2

--- src/views/public/Home.vue
+++ src/views/public/Home.vue
@@ -280,17 +280,18 @@ const handleSubmit = async () => {
 
   try {
     // Remplacez ces valeurs par les vôtres
-    await emailjs.send(
-      'service_i0uzyvl',      // ← Service ID depuis EmailJS
-      'template_4lziqus',     // ← Template ID depuis EmailJS
-      {
-        from_name: form.value.name,
-        from_email: form.value.email,
-        message: form.value.message,
-        to_name: 'NAO Graphics'
-      },
-      'TRI1CDVsowbwlcj1O'       // ← Public Key depuis EmailJS
-    )
+   await emailjs.send(
+  import.meta.env.VITE_EMAILJS_SERVICE_ID,
+  import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+  {
+    from_name: form.value.name,
+    from_email: form.value.email,
+    message: form.value.message,
+    to_name: 'NAO Graphics'
+  },
+  import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+)
+
 
     // Succès
     formSuccess.value = true
